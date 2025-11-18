f3a6777d1b832b15155f0e7b35f2c9a026e662c7
Sanskar <sanskar.dethe@gmail.com>
2025-11-18 23:05:52 +0530

    data ingestion notebook added

---
+++ main.py
@@ -0,0 +1,14 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.logging import logger
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>>> {STAGE_NAME} started <<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>> {STAGE_NAME} ended <<<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
